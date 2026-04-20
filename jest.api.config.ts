b3fa8f2449a52991f019fa1dd87f9968d8f3db61
Rose Allegrette <rallegre@umich.edu>
2026-04-20 11:06:51 -0700

    re-enable the API route tests

---
+++ jest.api.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+        jsx: "react-jsx",
+      },
+    ],
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  testMatch: ["**/__tests__/api/**/*.test.ts"],
+};
+
+export default config;
